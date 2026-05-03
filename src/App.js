a90d37c01170960cdae4e0de8a1e7a7186f7242c
Fabrice Theophile Diedhiou <fabricediedhiou3@gmail.com>
2026-05-03 02:15:48 +0000

    Setup initial SenTransport avec Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import LigneBus from './LigneBus';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<Header />
+<main className="contenu">
+<LigneBus
+numero="15"
+depart="Parcelles Assainies"
+arrivee="Plateau"
+arrets={14}
+/>
+<LigneBus
+numero="7"
+depart="Guediawaye"
+arrivee="Place Obe"
+arrets={18}
+/>
+</main>
+</div>
+);
 }
-
 export default App;
